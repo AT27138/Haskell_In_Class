9bcb38180f73b544c92df9420574808f7691e859
AT27138 <alex.taylor.u@gmail.com>
2026-03-19 09:58:20 +0000

    bean

--- first.hs
+++ first.hs
@@ -5,5 +5,14 @@ factorial 1 = 1
 factorial x = x * factorial ( pred x )
 
 
+isFactor :: (Int, Int) -> Bool
+isFactor (m, n) = ((m `mod` n) == 0)
+
 factors :: Int -> [Int]
-factors n = [x | x <- [1..n] | n mod x == 0]
+factors n = [x | x <- [1..n], isFactor (n, x)]
+
+isPrime :: Int -> Bool
+isPrime n = length ( factors n ) < 3
+
+primes :: Int -> [Int]
+primes n = [x | x <- [1..n], isPrime x]
